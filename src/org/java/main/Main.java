ad4d02052b7c087d92da30aa9aa79f3aac98da56
DarioGiusino <dario.giusino@gmail.com>
2023-05-17 11:41:40 +0200

    2nd milestone, todo rework with helpers

--- src/org/java/main/Main.java
+++ src/org/java/main/Main.java
@@ -1,17 +1,97 @@
 package org.java.main;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import org.java.obj.Event;
 
 public class Main {
 	public static void main(String[] args) {
+		// open scanner
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Benvenuto, inserisci un nuovo evento:" + "\nTitolo: ");
+		String title = sc.nextLine();
+
+		System.out.println("Data: (formato dd-MM-yyyy)");
+		String date = sc.nextLine();
+		String[] dateArr = date.split("-");
+		Integer day = Integer.valueOf(dateArr[0]);
+		Integer month = Integer.valueOf(dateArr[1]);
+		Integer year = Integer.valueOf(dateArr[2]);
+
+		System.out.print("Totale posti disponibili: ");
+		int totalSeats = sc.nextInt();
+
 		try {
-			Event e1 = new Event("Big fish", LocalDate.of(2023, 4, 18), 2000);
-			System.out.println(e1);
+			Event event = new Event(title, LocalDate.of(year, month, day), totalSeats);
+			System.out.println(event);
+
+			boolean ask = true;
+
+			while (ask) {
+				System.out.println(
+						"Cosa vuoi fare?" + "\n[1]Prenota posti" + "\n[2]Disdici posti" + "\n[3]Esci e Stampa");
+				int choice = sc.nextInt();
+
+				if (choice < 1 || choice > 3) {
+					System.err.println("Selezione non valida");
+					continue;
+				}
+
+				switch (choice) {
+				case 1: {
+					System.out.println("Quanti posti vuoi prenotare?");
+					int seats = sc.nextInt();
+
+					try {
+						for (int i = 0; i < seats; i++) {
+							event.AddSeats();
+						}
+						
+						System.out.println("Totale posti prenotati: "
+								+ event.getReservedSeats() + "\nTotale posti disponibili: "
+								+ (event.getTotalSeats() - event.getReservedSeats()));
+					} catch (Exception e) {
+						System.err.println(e);
+						continue;
+					}
+				}
+					break;
+
+				case 2: {
+					System.out.println("Quanti posti vuoi disdire?");
+					int seats = sc.nextInt();
+
+					try {
+						for (int i = 0; i < seats; i++) {
+							event.RemoveSeats();
+						}
+						
+						System.out.println("Totale posti prenotati: "
+								+ event.getReservedSeats() + "\nTotale posti disponibili: "
+								+ (event.getTotalSeats() - event.getReservedSeats()));
+					} catch (Exception e) {
+						System.err.println(e);
+						continue;
+					}
+				}
+					break;
+
+				case 3:
+					ask = false;
+					System.out.println("\n-------------------------------\n");
+					System.out.println("Grazie e arrivederci, ecco un riepilogo:");
+					System.out.println("Totale posti prenotati: "
+							+ event.getReservedSeats() + "\nTotale posti disponibili: "
+							+ (event.getTotalSeats() - event.getReservedSeats()));
+				}
+
+			}
+
 		} catch (Exception e) {
 			System.err.println("Error: " + e);
 		}
-		
+
 	}
 }
--- src/org/java/obj/Event.java
+++ src/org/java/obj/Event.java
@@ -5,82 +5,87 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Event {
-	
-	//variables
+
+	// variables
 	private String title;
 	private LocalDate date;
 	private int totalSeats;
 	private int reservedSeats = 0;
-	
-	//constructor
+
+	// constructor
 	public Event(String title, LocalDate date, int totalSeats) throws Exception {
 		setTitle(title);
 		setDate(date);
 		setTotalSeats(totalSeats);
 		setReservedSeats(reservedSeats);
 	}
-	
-	//title
+
+	// title
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	//date
+
+	// date
 	public LocalDate getDate() {
 		return date;
 	}
+
 	public String getFormattedDate() {
 		return date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
 	}
+
 	public void setDate(LocalDate date) throws Exception {
-		//check if date is in the past
+		// check if date is in the past
 		if (date.isBefore(LocalDate.now()))
-			throw new Exception ("The date cannot be in the past!");
-		
+			throw new Exception("The date cannot be in the past!");
+
 		this.date = date;
 	}
-	
-	//total seats
+
+	// total seats
 	public int getTotalSeats() {
 		return totalSeats;
 	}
+
 	private void setTotalSeats(int totalSeats) throws Exception {
-		//check if total seats is negative
-		if(totalSeats < 0)
-			throw new Exception ("Operation denied, criteria not fullfilled!");
-		
+		// check if total seats is negative
+		if (totalSeats < 0)
+			throw new Exception("Operation denied, criteria not fullfilled!");
+
 		this.totalSeats = totalSeats;
 	}
-	
-	//reserved seats
+
+	// reserved seats
 	public int getReservedSeats() {
 		return reservedSeats;
 	}
+
 	private void setReservedSeats(int reservedSeats) {
 		this.reservedSeats = reservedSeats;
 	}
-	
-	//add seats
+
+	// add seats
 	public void AddSeats() throws Exception {
-		//check if totalSeats are full or date is passed
-		if(getReservedSeats() == getTotalSeats() || getDate().isBefore(LocalDate.now()))
-			throw new Exception ("No more seats available or event passed!");
-		
-		setReservedSeats(reservedSeats++);
+		// check if totalSeats are full or date is passed
+		if (getReservedSeats() == getTotalSeats() || getDate().isBefore(LocalDate.now()))
+			throw new Exception("No more seats available or event passed!");
+
+		setReservedSeats(reservedSeats + 1);
 	}
-	
-	//remove seats
+
+	// remove seats
 	public void RemoveSeats() throws Exception {
-		//check if totalSeats are full or date is passed
-		if(getReservedSeats() == 0 || getDate().isBefore(LocalDate.now()))
-			throw new Exception ("This event contains no seats or event passed!");
-		
-		setReservedSeats(reservedSeats--);
+		// check if totalSeats are full or date is passed
+		if (getReservedSeats() == 0 || getDate().isBefore(LocalDate.now()))
+			throw new Exception("This event contains no seats or event passed!");
+
+		setReservedSeats(reservedSeats - 1);
 	}
-	
+
 	@Override
 	public String toString() {
 		return getFormattedDate() + " - " + getTitle();
